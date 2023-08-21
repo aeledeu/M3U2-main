2ec9f7d4a21805f734637833855a95db32eade48
Aldana Aradvari <aeledeu@hotmail.com>
2023-08-21 12:10:58 -0300

    ok

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -1,9 +1,9 @@
 var pool = require('./bd');
 
 async function getNovedades() {
-        var query = 'select * from novedades';
-        var rows = await pool.query(query);
-        return rows;
+    var query = 'select * from novedades';
+    var rows = await pool.query(query);
+    return rows;
 
 }
 
@@ -13,5 +13,33 @@ async function deleteNovedadesById(id) {
     return rows;
 }
 
+async function insertNovedad(obj) {
+    try {
+        var query = "insert into novedades set ?";
+        var rows = await pool.query(query, [obj]);
+        return rows;
 
-module.exports = { getNovedades, deleteNovedadesById}
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function getNovedadById(id) {
+    var query = "select * from novedades where id = ?";
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+async function modificarNovedadById(obj, id) {
+    try {
+        var query = "update novedades set ? where id=?";
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = { getNovedades, deleteNovedadesById, insertNovedad, getNovedadById, modificarNovedadById }
